b5cc8af03840a35bf2b6814737de67fdeec8c54b
georgeklangat <georgeklangat@gmail.com>
2025-01-22 05:59:09 +0300

    list rendering

--- Assets/main.js
+++ Assets/main.js
@@ -6,7 +6,16 @@ const app = Vue.createApp({
             image1: '/Assets/Images/socks_blue.jpg',
             image:'/Assets/Images/black_laptop.jpg',
             url: 'https://georgeslangat.netlify.app',
-            inStock: false
+            inStock: false,
+            details: ['50% cotton', '30% wool','20% polyester'],
+            variants:[
+                {id: 2234, color:'green'},
+                {id: 2235, color: 'blue'},
+            ],
+            sizes: [
+                {id:'black', size: [23,45,12,56,78,45]},
+                {id:'blue',size: [31,32,34,45,56,32]},
+            ]
 
         }
     }
